e3702fe979dbfe6ebba07f4ad32a864509ce2127
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-07 20:00:00 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -41,8 +41,8 @@ class CloneHelper:
         self.lsg = ""
         self.filee = ""
         self.u_id = self.mess.from_user.id
-        
-    
+        self.dname = ""
+    # self.dname = "" is addational
     def config(self):
         with open(
             'rclone.conf',
@@ -130,7 +130,7 @@ class CloneHelper:
             button.append(
                 [
                     pyrogram.InlineKeyboardButton(
-                        text="☁️ GDrive URL ☁️",
+                        text="☁️ Cloud URL ☁️",
                         url=f"{gau_link}"
                     )
             ]
@@ -145,7 +145,7 @@ class CloneHelper:
                 button.append([pyrogram.InlineKeyboardButton(text="ℹ️ Index URL ℹ️", url=f"{tam_link}")])
             button_markup = pyrogram.InlineKeyboardMarkup(button)
             msg = await self.lsg.edit_text(
-                f"🤖: {_up} Cloned successfully in @AbirHasan2005's Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
+                f"🤖: {_up} Cloned successfully in My Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
                 \n📀 Info: Calculating ...",
                 reply_markup=button_markup,
                 parse_mode="html"
@@ -168,7 +168,7 @@ class CloneHelper:
             LOGGER.info(am.decode('utf-8'))
             await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
             await msg.edit_text(
-                f"🤖: {_up} Cloned successfully in @AbirHasan2005's Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
+                f"🤖: {_up} Cloned successfully in My Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
                 \n📀 Info:\n{g_autam}",
                 reply_markup=button_markup,
                 parse_mode="html"
