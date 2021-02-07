1a56c9e36bd91384430a7271ff6547c0dcd98dd8
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-07 23:15:44 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -19,57 +19,65 @@ from tobrot import (
     MAX_MESSAGE_LENGTH,
     AUTH_CHANNEL
 )
+
+from tobrot.plugins.choose_rclone_config import rclone_button_callback
+
 async def button(bot, update: CallbackQuery):
     cb_data = update.data
+    LOGGER.info(cb_data)
     try:
         g = await AdminCheck(bot, update.message.chat.id, update.from_user.id)
-        print(g)
-    except:
-        pass
+        LOGGER.info(g)
+    except Exception as ee:
+        LOGGER.info(ee)
     if "|" in cb_data:
         await youtube_dl_call_back(bot, update)
-    elif (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
-        print(cb_data)
-        if cb_data.startswith("cancel"):
+    if cb_data.startswith("rclone"):
+        await rclone_button_callback(bot, update)
+        return
+    LOGGER.info(update.from_user.id)
+    LOGGER.info(update.message.reply_to_message.from_user.id)
+    if cb_data.startswith("cancel"):
+        if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
             if len(cb_data) > 1:
-                i_m_s_e_g = await update.message.reply_text("checking..?", quote=True)
+                i_m_s_e_g = await update.message.reply_to_message.reply_text("checking..?", quote=True)
                 aria_i_p = await aria_start()
                 g_id = cb_data.split()[-1]
                 LOGGER.info(g_id)
                 try:
                     downloads = aria_i_p.get_download(g_id)
+                    LOGGER.info(downloads)
                     file_name = downloads.name
                     LOGGER.info(downloads)
-                    LOGGER.info(downloads.remove(force=True))
+                    LOGGER.info(aria_i_p.remove(downloads = [downloads], force=True, files=True, clean=True))
                     if os.path.exists(file_name):
                         if os.path.isdir(file_name):
                             shutil.rmtree(file_name)
                         else:
                             os.remove(file_name)
-                    await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a> \n\n @AbirHasan2005 Please review them!. \n")
+                    await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
                 except Exception as e:
                     await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
-                else:
-                    await update.message.delete()
-        elif cb_data == "fuckingdo":
-            if update.from_user.id in AUTH_CHANNEL:
-                g_d_list = ['app.json', 'venv', 'rclone.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'DOWNLOADS', 'Procfile', '.heroku', '.profile.d', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'gautam', 'Torrentleech-Gdrive.log', 'vendor']
-                LOGGER.info(g_d_list)
-                g_list = os.listdir()
-                LOGGER.info(g_list)
-                g_del_list = list(set(g_list)-set(g_d_list))
-                LOGGER.info(g_del_list)
-                if len(g_del_list) != 0:
-                    for f in g_del_list:
-                        if os.path.isfile(f):
-                            os.remove(f)
-                        else:
-                            shutil.rmtree(f)
-                    await update.message.edit_text(f"Deleted {len(g_del_list)} objects 😬 \n\n @AbirHasan2005 Please review them!. \n")
-                else:
-                    await update.message.edit_text("Nothing to clear 🙄 \n\n @AbirHasan2005 Please review them!. \n")
+                # else:
+                #     await update.message.delete()
+    elif cb_data == "fuckingdo":
+        if update.from_user.id in AUTH_CHANNEL:
+            g_d_list = ['app.json', 'venv', 'rclone.conf', 'rclone_bak.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'Procfile', '.heroku', '.profile.d', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'gautam', 'Torrentleech-Gdrive.txt', 'vendor', 'LeechBot.session', 'LeechBot.session-journal']
+            LOGGER.info(g_d_list)
+            g_list = os.listdir()
+            LOGGER.info(g_list)
+            g_del_list = list(set(g_list)-set(g_d_list))
+            LOGGER.info(g_del_list)
+            if len(g_del_list) != 0:
+                for f in g_del_list:
+                    if os.path.isfile(f):
+                        os.remove(f)
+                    else:
+                        shutil.rmtree(f)
+                await update.message.edit_text(f"Deleted {len(g_del_list)} objects 😬")
             else:
-                await update.message.edit_text("You are not allowed to do that !! \n\n @AbirHasan2005 Please review them!. \n")
-        elif cb_data == "fuckoff":
-            await update.message.edit_text("Okay! Fine.")
-				
+                await update.message.edit_text("Nothing to clear 🙄")
+        else:
+            await update.message.edit_text("You are not allowed to do that 🤭")
+    elif cb_data == "fuckoff":
+        await update.message.edit_text("Okay! fine 🤬")
