54b889db60ef10621b36552e36e60f865af68d9d
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-07 23:04:52 +0530

    Update download.py

--- tobrot/helper_funcs/download.py
+++ tobrot/helper_funcs/download.py
@@ -26,12 +26,11 @@ from tobrot import (
 )
 from tobrot.helper_funcs.display_progress_g import progress_for_pyrogram_g
 from tobrot.helper_funcs.upload_to_tg import upload_to_gdrive
-from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function_t
 from tobrot.helper_funcs.create_compressed_archive import unzip_me, unrar_me, untar_me
 
 async def down_load_media_f(client, message):
     user_id = message.from_user.id
-    print(user_id)
+    LOGGER.info(user_id)
     mess_age = await message.reply_text("...", quote=True)
     if not os.path.isdir(DOWNLOAD_LOCATION):
         os.makedirs(DOWNLOAD_LOCATION)
@@ -39,14 +38,17 @@ async def down_load_media_f(client, message):
         start_t = datetime.now()
         download_location = "/app/"
         c_time = time.time()
-        the_real_download_location = await client.download_media(
-            message=message.reply_to_message,
-            file_name=download_location,
-            progress=progress_for_pyrogram_g,
-            progress_args=(
-                "Trying to download! Please Wait ...", mess_age, c_time
+        try:
+            the_real_download_location = await client.download_media(
+                message=message.reply_to_message,
+                file_name=download_location,
+                progress=progress_for_pyrogram_g,
+                progress_args=(
+                    "trying to download", mess_age, c_time
+                )
             )
-        )
+        except Exception as g_e:
+            LOGGER.info(g_e)
         end_t = datetime.now()
         ms = (end_t - start_t).seconds
         LOGGER.info(the_real_download_location)
@@ -55,19 +57,19 @@ async def down_load_media_f(client, message):
         the_real_download_location_g = os.path.basename(the_real_download_location)
         LOGGER.info(the_real_download_location_g)
         if len(message.command) > 1:
-            if message.command[1] == "unzip":
+            if message.command[1].lower() == "unzip":
                 file_upload = await unzip_me(the_real_download_location_g)
                 if file_upload is not None:
                     g_response = await upload_to_gdrive(file_upload, mess_age, message, user_id)
                     LOGGER.info(g_response)
                     
-            elif message.command[1] == "unrar":
+            elif message.command[1].lower() == "unrar":
                 file_uploade = await unrar_me(the_real_download_location_g)
                 if file_uploade is not None:
                     gk_response = await upload_to_gdrive(file_uploade, mess_age, message, user_id)
                     LOGGER.info(gk_response)
                     
-            elif message.command[1] == "untar":
+            elif message.command[1].lower() == "untar":
                  file_uploadg = await untar_me(the_real_download_location_g)
                  if file_uploadg is not None:
                      gau_response = await upload_to_gdrive(file_uploadg, mess_age, message, user_id)
@@ -76,9 +78,10 @@ async def down_load_media_f(client, message):
             gaut_response = await upload_to_gdrive(the_real_download_location_g, mess_age, message, user_id)
             LOGGER.info(gaut_response)
     else:
-        #await asyncio.sleep(4)
-        await mess_age.edit_text("Reply to a Telegram Media, to upload to My Cloud Drive.")
-# next will be new
+        await mess_age.edit_text("Reply to a Telegram Media, to upload to the Cloud Drive.")
+
+
+
 async def download_tg(client, message):
     user_id = message.from_user.id
     LOGGER.info(user_id)
