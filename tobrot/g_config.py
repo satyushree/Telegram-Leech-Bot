7565b0938343d0d64944d2a01ab284ca076205cb
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-24 21:34:24 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -9,17 +9,15 @@ class Config(Config):
     APP_ID = 1309280
     API_HASH = "af327dd857e0e65f80fefcf6d0af4afd"
     OWNER_ID = 1243382770
-    AUTH_CHANNEL = [-1001195135562]
+    AUTH_CHANNEL = [-1001485521459,1087968824,1243382770]
     DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
-[ffmg]
-type = drive\n
-client_id = 437611717237-cohifba2pi09vdkg8i4osf9t58tqj8er.apps.googleusercontent.com\n
-client_secret = 1QAQdobw0XXAAfZjRn0ITF6w\n
-scope = drive\n
-root_folder_id = 1Ss-FUl2rRaTiLk1lq74yAcmj5i8dPJXj\n
-token = {"access_token":"ya29.A0AfH6SMA9J7PDzd-5V-8OlOcfVO4Mc_GZSQ-0PsnQWuXZXTIcka2ljreP9cRLvkPxI_rlbR6LHGSafMHt7ZVjSA4reSNAehEw00lAR8_-QgxUa-dUqr_ocVHLbS84lPCzKIH8P7lAWdwzDwVte1u0yH-0Hjzd","token_type":"Bearer","refresh_token":"1//0gNvii4scilMBCgYIARAAGBASNwF-L9IroVv4pg_aTu627eGOpNv7HjRxjUOof_OFCxGOdHA2Af2tUgB0YsRGsxrSdVRMBNNKyqc","expiry":"2021-02-07T18:59:45.7438573+05:30"}\n
-team_drive = 0AKM0e85jWxEpUk9PV
+[TorrentGenerator_Bot]
+type = drive
+scope = drive
+root_folder_id = 1Ss-FUl2rRaTiLk1lq74yAcmj5i8dPJXj
+token = {"access_token":"ya29.A0AfH6SMCnpzkVSL96IiMcNKef1Q0tAgqaJ9UqIIf-vBqzNQ2911iGQE5pJAjIapCipbtlC441sHStj84tXXPRxwE_qxb7E2E3xwq79wobky6Ow6flhk366Ld1LXncVodgJDke0lUV8c5saRLBimrn0pgsmHvb","token_type":"Bearer","refresh_token":"1//0g4JPYyDMkDucCgYIARAAGBASNwF-L9IrebVKrFnIdxrCVqK1Id-Ot6qdZPPSg-Bkn3iqIg07KkpPcE2ESiuS25Ymk7p4gGsJccY","expiry":"2021-02-24T19:54:46.0239209+05:30"}
+team_drive = 0AKM0e85jWxEpUk9PVA
 """
