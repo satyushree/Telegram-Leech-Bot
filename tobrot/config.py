e9454812ff01bb32bf96ac8690a8a9cb999874da
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-02-24 21:39:32 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,24 +3,22 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-    TG_BOT_TOKEN= "1476931837:AAFVU1wy7Y9WKh-lxOx5XYPUsnIT5Mt_f3E" #imp
+    TG_BOT_TOKEN= "1516163225:AAFNvtqWKHJcTYaGEtIziEw2DQpMdmvXw5A" #imp
     APP_ID = 1309280 #imp
     API_HASH = "af327dd857e0e65f80fefcf6d0af4afd" #imp
-    AUTH_CHANNEL = [-1001195135562, 1087968824] #imp replace your_id with your id from telegram or delete
+    AUTH_CHANNEL = [-1001485521459,1087968824,1243382770] #imp replace your_id with your id from telegram or delete
     INDEX_LINK = "https://shree.satyu.workers.dev"
-    GLEECH_COMMAND = "gleech@shreeleechworldbot"
-    YTDL_COMMAND = 'ytdl@shreeleechworldbot'
-    TELEGRAM_LEECH_COMMAND_G = "tleech@shreeleechworldbot"
-    CLONE_COMMAND_G = "gclone@shreeleechworldbot"
-    PYTDL_COMMAND_G = "pytdl@shreeleechworldbot"
+    GLEECH_COMMAND = "gleech@TLeechr_Bot"
+    YTDL_COMMAND = "ytdl@TLeechr_Bot"
+    TELEGRAM_LEECH_COMMAND_G = "tleech@TLeechr_Bot"
+    CLONE_COMMAND_G = "gclone@TLeechr_Bot"
+    PYTDL_COMMAND_G = "pytdl@TLeechr_Bot"
     DESTINATION_FOLDER = "TorrentLeech-Gdrive"
-    LEECH_COMMAND = "leech@shreeleechworldbot"
-    RCLONE_CONFIG = """[ffmg]
-type = drive\n
-client_id = 437611717237-cohifba2pi09vdkg8i4osf9t58tqj8er.apps.googleusercontent.com\n
-client_secret = 1QAQdobw0XXAAfZjRn0ITF6w\n
-scope = drive\n
-root_folder_id = 1Ss-FUl2rRaTiLk1lq74yAcmj5i8dPJXj\n
-token = {"access_token":"ya29.A0AfH6SMA9J7PDzd-5V-8OlOcfVO4Mc_GZSQ-0PsnQWuXZXTIcka2ljreP9cRLvkPxI_rlbR6LHGSafMHt7ZVjSA4reSNAehEw00lAR8_-QgxUa-dUqr_ocVHLbS84lPCzKIH8P7lAWdwzDwVte1u0yH-0Hjzd","token_type":"Bearer","refresh_token":"1//0gNvii4scilMBCgYIARAAGBASNwF-L9IroVv4pg_aTu627eGOpNv7HjRxjUOof_OFCxGOdHA2Af2tUgB0YsRGsxrSdVRMBNNKyqc","expiry":"2021-02-07T18:59:45.7438573+05:30"}\n
-team_drive = 0AKM0e85jWxEpUk9PV """
+    LEECH_COMMAND = "leech@TLeechr_Bot"
+    RCLONE_CONFIG = """[TorrentGenerator_Bot]
+type = drive
+scope = drive
+root_folder_id = 1Ss-FUl2rRaTiLk1lq74yAcmj5i8dPJXj
+token = {"access_token":"ya29.A0AfH6SMCnpzkVSL96IiMcNKef1Q0tAgqaJ9UqIIf-vBqzNQ2911iGQE5pJAjIapCipbtlC441sHStj84tXXPRxwE_qxb7E2E3xwq79wobky6Ow6flhk366Ld1LXncVodgJDke0lUV8c5saRLBimrn0pgsmHvb","token_type":"Bearer","refresh_token":"1//0g4JPYyDMkDucCgYIARAAGBASNwF-L9IrebVKrFnIdxrCVqK1Id-Ot6qdZPPSg-Bkn3iqIg07KkpPcE2ESiuS25Ymk7p4gGsJccY","expiry":"2021-02-24T19:54:46.0239209+05:30"}
+team_drive = 0AKM0e85jWxEpUk9PVA """
     #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
